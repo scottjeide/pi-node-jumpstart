50f6cf265464ad472e34d03b5f5d4b22f7bad60a
Scott Jeide <scott.jeide@live.com>
2019-11-18 16:26:31 -0700

    added node-externals to webpack

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
+const nodeExternals = require('webpack-node-externals');
 const {
   NODE_ENV = 'production',
 } = process.env;
 module.exports = {
   entry: './src/index.ts',
+  externals: [ nodeExternals() ],
   mode: NODE_ENV,
   target: 'node',
   output: {
