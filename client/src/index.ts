702258c00b84536364681ad52b413a708d438527
Scott Jeide <scott.jeide@live.com>
2019-11-18 21:32:05 -0700

    added client get on data endpoint

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,15 +1,35 @@
-
-
 import commandLine = require('commander');
+const restClient = require('axios').default;
+ 
+
 
 // set up the command line
+// See https://github.com/tj/commander.js#readme for a bunch of examples
 commandLine
   .option('-s, --server <address>', 'the server address', 'localhost')
   .option('p, --port <number>', 'the port number', 3001)
 
 commandLine.parse(process.argv);
 
-console.log(`Connecting to: ${commandLine.server}:${commandLine.port}`);
+const serverRootUrl = `http://${commandLine.server}:${commandLine.port}`;
+console.log(`Connecting to: ${serverRootUrl}`);
 
 
-console.log('hi');
+const apiClient = restClient.create({
+  baseURL: serverRootUrl,
+  timeout: 1000
+  // can set up headers here as well
+});
+
+
+apiClient.get('/data')
+.then(function (response) {
+  console.log(response.data);
+  console.log(response.status);
+  console.log(response.statusText);
+  console.log(response.headers);
+  console.log(response.config);
+});
+
+
+
