cf67bbf1a228c045cf367d396ef3121784b43064
Scott Jeide <scott_jeide@intuit.com>
2020-03-08 13:53:47 -0600

    still plugging along on server changes

--- server/src/index.ts
+++ server/src/index.ts
@@ -24,94 +24,130 @@ const redis = new Redis(); // this will connect to 127.0.0.1:6379
 // TODO: Need to figure out how to detect if redis is down and handle it gracefully
 // Looks like you can specify lazyConnect = true, then call async redis.connect() and handle the exception but haven't tried it yet
 
-var res = redis.set("test:startupMsg", "Hello! Server started at " + new Date());
-console.log("wrote startup message: " + JSON.stringify(res));
+//var res = redis.set("test:startupMsg", "Hello! Server started at " + new Date());
+//console.log("wrote startup message: " + JSON.stringify(res));
 
-
-
-// See if we can define the apis in some sort of a basic schema
-const schema = {
-  // the endpoint for the data we are collecting
-  data: {
-    // maybe add some type information in here too?
-    fields: [
-      'reading1',
-      'reading2'
-    ],
-
-    // something to flag it as N items (as opposed to the controlPanel which will normally only have one)
-
-    // and a temporary hack to hold entries in memory until I wire up the db
-    db: [{reading1: 123, reading2:321}, {reading1: 654, reading2:456}]
-  },
-
-
-  // the endpoint for client runtime messages. Messages that clients post to this will be added to the runtime
-  // messages table and can be viewed in the UI
-  messages: {
-    fields: [
-      'text',
-    ]
-  },
-
-
-  // the endpoint for the controlPanel / settings
-  controlPanel: {
-    fields: [
-      'run',
-      'status'
-    ],
-
-    // maybe something here to flag is as there should only be one item
-
-    // temp 'db'
-    db: [{run: false, status:''}]
-
-  }
-
-
-
-};
 
 // Set up the controlPanel api handlers
 expressApp.get('/controlPanel', (req, res) => {
   res.send(dataDefinitions.controlPanel);
 });
 expressApp.post('/controlPanel', (req, res) => {
-  // set everything except the run-id 
-  if (req.body.) {
-    
+  const newSettings = {...req.body};
+  
+  console.log("Received new controlPanel setting: " + JSON.stringify(newSettings));
+
+  // the server controls the runId property
+  if (newSettings.on && !dataDefinitions.controlPanel.on) {
+    // starting a new run so create a new rid
+    newSettings.runId = (new Date).toString();
+    console.log("Starting new runId: " + newSettings.runId);
   }
-  // echo it back
+  else {
+    delete newSettings.runId;
+  }
+
+  for (let prop in dataDefinitions.controlPanel) {
+    if (newSettings.hasOwnProperty(prop)) {
+        dataDefinitions.controlPanel[prop] = newSettings[prop];
+    }
+  }
+  
+  // Save off the runid in the db
+  const dbRes = redis.set("runId:" + dataDefinitions.controlPanel.runId, JSON.stringify(dataDefinitions.controlPanel));
+  console.log("Saved runId, res: " + dbRes);
+
+  // echo it back on the post request response
   res.send(dataDefinitions.controlPanel);
+
+  // and broadcast it on the socket
+  socketIo.emit('io:controlPanel', {emit: true, msg: dataDefinitions.controlPanel});
+});
+
+
+// The measurement api handlers
+expressApp.get('/measurement', (req, res) => {
+  //res.send(dataDefinitions.controlPanel);
+});
+expressApp.post('/measurement', (req, res) => {
+  const measurement = {...req.body};
+  
+  console.log("Received new measurement: " + JSON.stringify(measurement));
+  
+  const now = new Date();
+  // remove anything that shouldn't be there just in case
+  for (let prop in measurement) {
+    if (dataDefinitions.measurement.hasOwnProperty(prop)) {
+      // valid measurement so log it
+      
+      // Probably would be best to just write the all in one xadd call, but try this first
+
+      const dbRet = redis.xadd(
+        'measurement:' + dataDefinitions.controlPanel.runId, 
+        '*', 
+        prop, measurement.prop, 
+        'time', now.toString()
+        );
+      console.log("Measurement save ret: " + dbRet);
+    }
+    else {
+      delete measurement.prop;
+    }
+  }
+
+  // echo it back on the post request response
+  res.send(measurement);
+
+  // and broadcast it on the socket
+  socketIo.emit('io:measurement', {emit: true, msg: measurement});
+});
+
+
+// The message api handlers
+expressApp.get('/runtimeMessage', (req, res) => {
+  //res.send(dataDefinitions.controlPanel);
+});
+expressApp.post('/runtimeMessage', (req, res) => {
+  const runtimeMessage = {...req.body};
+  
+  console.log("Received new message: " + JSON.stringify(runtimeMessage));
+  
+  const now = new Date();
+
+  // remove anything that shouldn't be there just in case
+  for (let prop in runtimeMessage) {
+    if (!dataDefinitions.runtimeMessage.hasOwnProperty(prop)) {
+      delete runtimeMessage.prop;
+    }
+  }
+
+  const dbRet = redis.xadd(
+    'runtimeMessage:' + dataDefinitions.controlPanel.runId, 
+    '*', 
+    'text', runtimeMessage.text,
+    'time', now.toString()
+    );
+  console.log("Measurement save ret: " + dbRet);
+
+  // echo it back on the post request response
+  res.send(runtimeMessage);
+
+  // and broadcast it on the socket
+  socketIo.emit('io:runtimeMessage', {emit: true, msg: runtimeMessage});
 });
 
 
 
-// Set up a get handler for each of the endpoints
-for(const endpoint in schema) {
-  expressApp.get('/' + endpoint, (req, res) => {
-    res.send(schema[endpoint].db);
-  });
-
-  /*
-  app.put('/' + endpoint, (req, res) => {
-  });
-  */
-}
-
 // set up the socket handler
 socketIo.on('connection', function(socket){
   console.log('Got new client connection');
-  redis.xadd('server:runLog', '*', 'message', 'New client connection');
+//  redis.xadd('server:runLog', '*', 'message', 'New client connection');
 
-  socket.on('clientMessage', function(msg){
-    var t = new Date();
-    redis.xadd('client:runLog', '*', 'message', JSON.stringify(msg), 'time', t.toString());
-    console.log('Got client message: ' + JSON.stringify(msg));
+  socket.on('io:runtimeMessage', function(msg){
+    console.log('Got runtimeMessage: ' + JSON.stringify(msg));
 
     // broadcast it back out to everyone (including the original sender)
-    socketIo.emit('clientMessage', {emit: true, msg: msg});
+    //socketIo.emit('clientMessage', {emit: true, msg: msg});
 
     // this version will broadcast back to everyone _except_ for the original sender
     //socket.broadcast.emit('clientMessage', {broadcast: true, msg: msg});
