9bc4ba8e5572a0e2430191aa8252ba41ac7a717b
Scott Jeide <scott.jeide@live.com>
2020-03-26 17:52:27 -0600

    starting to do some dynamic updates on control panel

--- ui/src/components/controlPanel.vue
+++ ui/src/components/controlPanel.vue
@@ -4,7 +4,7 @@
     <v-divider></v-divider>
     <v-switch
         v-model="onOff"
-        :label="On/Off"
+        :label="`Power: ${onOff ? 'On' : 'Off'}`"
       ></v-switch>
   </div>
 </template>
@@ -12,23 +12,30 @@
 <script lang="ts">
 console.log('panel opened');
 
-// Listen for any controlPanel/settings changes
-/*
-import * as dataDefinitions from '../../../shared/dataDefinitions';
+import * as dataDefinitions from '../../../shared/dataDefinitions'; // eslint-disable-line no-unused-vars
 let currentSettings: dataDefinitions.controlPanel = {
   on: false,
   runId: '',
   smokerSetTemp: 0,
 };
-*/
 
 
 export default {
   data: function() {
     return {
-      someValue: 'test'
+      onOff: currentSettings.on
     }
-  },
+  }, 
+  watch: {
+    onOff: (val) => {
+      console.log('on/off changed', val);
+
+      // It is not directly updating current settings. The state is only held in the onOff value in data
+      // Looks like it might be best to just have data values for each of the things (runid, on, temp)
+      // and watch for changes on them and then save to server as they change
+      console.log('currentSettings:', currentSettings)
+    }
+  } 
   // can we do props here too? Like passing in a URL or something?
 }
 </script>
